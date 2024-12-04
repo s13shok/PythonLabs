3cc580c60c57bb66de2d90a9715242086bb550ce
Andrew Polous <s13shok@gmail.com>
2024-12-04 22:57:14 +0300

    Test commit for TeamCity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Calculator():
     def plus(self, a:int, b:int):
         if not isinstance(a, int) or not isinstance(b, int):
-            raise Exception("No int")
+            raise Exception('No int')
         return a+b
